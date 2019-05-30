e27448d776135b2db81d4ddbaa26af865d0f6940
Parker Cooper <parkr.coopr@gmail.com>
2019-05-30 15:39:13 -0400

    fixed loaders, added build script

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Navbar = () => {
+  return <div className="navbar">Navbar</div>;
+};
+
+export default Navbar;
